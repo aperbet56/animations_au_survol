e05bf7a3ed3b6ec95a104a44c0757068d0f9f784
Annabelle Perbet <annabelleperbet@yahoo.fr>
2023-11-26 16:00:00 +0100

    Ajout du fichier script.js

---
+++ script.js
@@ -0,0 +1,23 @@
+// Récupération des différents éléments
+const btn1 = document.querySelector(".btn__1");
+const btn2 = document.querySelector(".btn__2");
+
+// Ecoute de l'événement mouseenter sur le bouton 1
+btn1.addEventListener("mouseenter", () => {
+  btn2.classList.add("blurred");
+});
+
+// Ecoute de l'événement mouseleave sur le bouton 1
+btn1.addEventListener("mouseleave", () => {
+  btn2.classList.remove("blurred");
+});
+
+// Ecoute de l'événement mouseenter sur le bouton 2
+btn2.addEventListener("mouseenter", () => {
+  btn1.classList.add("blurred");
+});
+
+// Ecoute de l'événement mouseleave sur le bouton 2
+btn2.addEventListener("mouseleave", () => {
+  btn1.classList.remove("blurred");
+});
