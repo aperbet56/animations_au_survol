171b848bbccb3bfbaafdb5b61e74a1d8dca47724
Annabelle Perbet <annabelleperbet@yahoo.fr>
2023-11-26 16:17:39 +0100

    Ajout du code js pour l'animation text glow

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Récupération des différents éléments
+// Récupération des deux boutons
 const btn1 = document.querySelector(".btn__1");
 const btn2 = document.querySelector(".btn__2");
 
@@ -20,4 +20,17 @@ btn2.addEventListener("mouseenter", () => {
 // Ecoute de l'événement mouseleave sur le bouton 2
 btn2.addEventListener("mouseleave", () => {
   btn1.classList.remove("blurred");
+});
+
+// Récupération de la div ayant la classe text
+const txt = document.querySelector(".txt");
+
+// Ecoute de l'événement mouseenter sur la div
+txt.addEventListener("mouseenter", () => {
+  txt.classList.add("animation");
+});
+
+// Ecoute de l'événement mouseleave sur la div
+txt.addEventListener("mouseleave", () => {
+  txt.classList.remove("animation");
 });
